eafd0b34421a3855833629db73218b99ceff79d3
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-24 12:39:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ if uploaded_file:
             stats_qualita.columns = ['Stato Nota', 'Conteggio']
             
             # --- PRIMA RIGA: GRAFICO A TORTA TIPOLOGIE ---
-            col1, col2 = st.columns([2, 2]) 
+            col1, col2 = st.columns([3, 1]) 
             with col1:
                 st.write("#### Tipologie Eventi")
                 fig_pie_tipo = px.pie(
@@ -126,6 +126,8 @@ if uploaded_file:
 
             with col2: 
                 totale_attivita = len(df_filtrato)
+                st.write("")
+                st.write("")
                 st.metric("Totale Attività", totale_attivita)
                 st.dataframe(stats_tipo, hide_index=True, use_container_width=True)
                 

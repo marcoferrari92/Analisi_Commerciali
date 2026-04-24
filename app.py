c44f6ac827264c2378125532654b1cc3cdfd4594
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-24 10:36:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -126,9 +126,11 @@ if uploaded_file:
             st.write("#### Analisi Qualità Note")
             with st.popover("💡 Analisi"):
                     st.info(""" 
-                    Alcuni eventi sono privi di note e non apportano contenuto informativo (Eventi MUTI).
-                    * **Tip 1:** mettere un vincolo nel CRM per cui eventi senza note non possono essere caricati.
-                    * **Tip 2:** molti eventi hanno note poco comprensibili. Strutturare il campo note con le classiche 5 W del giornalismo sarebbe utile.
+                    Controlliamo la qualità degli eventi inseriti per il famoso teorema: *garbade input, garbage output*.
+                    * ⚠️**Issue 1:** Alcuni eventi sono privi di note e non apportano contenuto informativo (Eventi MUTI).
+                        * 💡*Tip:* mettere un vincolo nel CRM per cui eventi senza note non possono essere caricati.
+                    * ⚠️**Issue 2:** molti eventi hanno note poco comprensibili.
+                        * 💡*Tip:* strutturare il campo note con le classiche 5 W del giornalismo sarebbe utile.
                     """)
 
             fig_pie_qual = px.pie(

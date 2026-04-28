d1b84b4747b772ad3001e9d545da8ba6a8e9ef7b
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-28 13:14:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def pie_ordini(df):
     # 2. Creazione Grafico a Torta
     if not conteggio.empty:
 
-        col1, col2, col3 = st.columns([2, 0.5, 1])
+        col1, col2, col3, col4 = st.columns([1.5, 0.5, 1, 0.5])
 
         with col1:
 

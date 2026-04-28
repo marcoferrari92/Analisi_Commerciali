63d9f90e081191054297d309c74505c23e312a81
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-28 19:59:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def render_grafico_torta(data, values_col, names_col, titolo, tipo="numerico"):
             traceorder="normal"
         ),
         margin=dict(t=100, b=20, l=20, r=20),
-        title_x=0 
+        title_x=0.3 
     )
     
     st.plotly_chart(fig, use_container_width=True)

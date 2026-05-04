1f68fa505ad331124716b6f98d28b34229add708
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-05-04 12:02:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,7 @@ def plot_distribuzione_ordini(df_target):
                 xbins=dict(
                     start=0,      # Punto di inizio del primo bin
                     end=100000,   # Punto di fine (opzionale)
-                    size=500      # LARGHEZZA DEL BIN (es. ogni bin copre 500€)
+                    size=1000      # LARGHEZZA DEL BIN (es. ogni bin copre 500€)
                 ),
                 marker_line=dict(width=1, color='white'),
                 legendgroup=stadio

8045f576c5a8a9c131601eaa35c589c0ba812e23
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-05-06 10:14:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -447,7 +447,7 @@ def analisi_conversione_preventivi(df, finestra, giorni_scadenza=7):
                           color='STATO_FINALE', color_discrete_map=color_map_stato)
         fig_pie_n.update_traces(
             textinfo='value+percent', 
-            texttemplate='%{value}<br>**%{percent}**'
+            texttemplate='%{value}<br><b>%{percent}<b>'
         )
         fig_pie_n.update_layout(legend=dict(orientation="h", y=-0.1, x=0.5, xanchor="center"))
         st.plotly_chart(fig_pie_n, use_container_width=True)
@@ -459,7 +459,7 @@ def analisi_conversione_preventivi(df, finestra, giorni_scadenza=7):
                             color='STATO_FINALE', color_discrete_map=color_map_stato)
         fig_pie_val.update_traces(
             textinfo='value+percent',
-            texttemplate='€%{value:,.2f}<br>**%{percent}**'
+            texttemplate='€%{value:,.2f}<br><b>%{percent}<b>'
         )
         fig_pie_val.update_layout(legend=dict(orientation="h", y=-0.1, x=0.5, xanchor="center"))
         st.plotly_chart(fig_pie_val, use_container_width=True)
